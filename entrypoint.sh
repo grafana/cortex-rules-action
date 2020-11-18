8f2be4a1a64981b69d392fdefed1dae74b70f228
RyanW8 <r.woods@node4.co.uk>
2020-11-18 17:41:32 +0000

    Init

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,9 +27,19 @@ SYNC_CMD=sync
 DIFF_CMD=diff
 PRINT_CMD=print
 
+if [ -z "${COMPONENT}" ]; then
+  echo "COMPONENT not set, select either RULER or ALERTMANAGER"
+  exit 1
+fi
+
 if [ -z "${RULES_DIR}" ]; then
   echo "RULES_DIR not set, using './' as a default."
   RULES_DIR="./"
+fi
+
+if [ ${COMPONENT} == "ALERTMANAGER" ] && [ -z "${ALERTMANAGER_CONFIG_PATH}" ]; then
+  echo "No ALERTMANAGER_CONFIG_PATH variable set. Defaulting to ./alertManager.yml"
+  ALERTMANAGER_CONFIG_PATH=./alertManager.yml
 fi
 
 if [ -z "${ACTION}" ]; then
@@ -37,38 +47,46 @@ if [ -z "${ACTION}" ]; then
   exit 1
 fi
 
-case "${ACTION}" in
-  $SYNC_CMD)
-    verifyTenantAndAddress
-    OUTPUT=$(/usr/bin/cortextool rules sync --rule-dirs="${RULES_DIR}" "$@")
-    STATUS=$?
-    ;;
-  $DIFF_CMD)
-    verifyTenantAndAddress
-    OUTPUT=$(/usr/bin/cortextool rules diff --rule-dirs="${RULES_DIR}" "$@")
-    STATUS=$?
-    ;;
-  $LINT_CMD)
-    OUTPUT=$(/usr/bin/cortextool rules lint --rule-dirs="${RULES_DIR}" "$@")
-    STATUS=$?
-    ;;
-  $PREPARE_CMD)
-    OUTPUT=$(/usr/bin/cortextool rules prepare -i --rule-dirs="${RULES_DIR}" "$@")
-    STATUS=$?
-    ;;
-  $CHECK_CMD)
-    OUTPUT=$(/usr/bin/cortextool rules check --rule-dirs="${RULES_DIR}" "$@")
-    STATUS=$?
-    ;;
-  $PRINT_CMD)
-      OUTPUT=$(/usr/bin/cortextool rules print "$@")
+if [ "${COMPONENT}" == "RULER" ]; then
+  case "${ACTION}" in
+    $SYNC_CMD)
+      verifyTenantAndAddress
+      OUTPUT=$(/usr/bin/cortextool rules sync --rule-dirs="${RULES_DIR}" "$@")
       STATUS=$?
       ;;
-  *)
-    err "Unexpected action '${ACTION}'"
-    exit 1
-    ;;
-esac
+    $DIFF_CMD)
+      verifyTenantAndAddress
+      OUTPUT=$(/usr/bin/cortextool rules diff --rule-dirs="${RULES_DIR}" "$@")
+      STATUS=$?
+      ;;
+    $LINT_CMD)
+      OUTPUT=$(/usr/bin/cortextool rules lint --rule-dirs="${RULES_DIR}" "$@")
+      STATUS=$?
+      ;;
+    $PREPARE_CMD)
+      OUTPUT=$(/usr/bin/cortextool rules prepare -i --rule-dirs="${RULES_DIR}" "$@")
+      STATUS=$?
+      ;;
+    $CHECK_CMD)
+      OUTPUT=$(/usr/bin/cortextool rules check --rule-dirs="${RULES_DIR}" "$@")
+      STATUS=$?
+      ;;
+    $PRINT_CMD)
+        OUTPUT=$(/usr/bin/cortextool rules print "$@")
+        STATUS=$?
+        ;;
+    *)
+      err "Unexpected action '${ACTION}'"
+      exit 1
+      ;;
+  esac
+elif [ "${COMPONENT}" == "ALERTMANAGER" ]; then
+  OUTPUT=$(/usr/bin/cortextool alertmanager load ${ALERTMANAGER_CONFIG_PATH})
+  STATUS=$?
+else
+  echo "Invalid component selected, use RULER or ALERTMANAGER"
+  exit 1
+fi
 
 echo "${OUTPUT}"
 echo ::set-output name=detailed::"${OUTPUT}"
