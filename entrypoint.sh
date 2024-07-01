210223706db53d80d0624866573d421ca771b13d
Marco Pracucci <marco@pracucci.com>
2024-07-01 08:42:37 +0200

    Apply suggestions from code review

--- entrypoint.sh
+++ entrypoint.sh
@@ -77,8 +77,8 @@ case "${ACTION}" in
 esac
 
 SINGLE_LINE_OUTPUT=$(echo "${OUTPUT}" | awk 'BEGIN { RS="" } { gsub(/%/, "%25"); gsub(/\r/, "%0D"); gsub(/\n/, "%0A") } { print }')
-echo "detailed=${SINGLE_LINE_OUTPUT}" >> $GITHUB_OUTPUT
+echo "detailed=${SINGLE_LINE_OUTPUT}" >> "$GITHUB_OUTPUT"
 SUMMARY=$(echo "${OUTPUT}" | grep Summary)
-echo "summary=${SUMMARY}" >> $GITHUB_OUTPUT
+echo "summary=${SUMMARY}" >> "$GITHUB_OUTPUT"
 
 exit $STATUS
